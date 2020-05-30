0e84079fcc97666a9db4f9d32f6327c5c49af898
Noah Masur <noah.masur@take2games.com>
2020-05-30 15:36:05 -0600

    Working all regions futures

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,106 @@
+use futures::stream::futures_unordered::FuturesUnordered;
+use futures::stream::StreamExt;
 use rusoto_core::Region;
-use rusoto_ec2::{DescribeInstancesRequest, Ec2, Ec2Client, Instance};
+use rusoto_ec2::{DescribeInstancesRequest, DescribeInstancesResult, Ec2, Ec2Client, Instance};
+
+struct InstanceMetadata {
+    name: String,
+    ip: String,
+    region: Region,
+}
 
 #[tokio::main]
 async fn main() {
-    // let regions: [Region; 4] = [
-    //     Region::UsEast1,
-    //     Region::UsEast2,
-    //     Region::UsWest1,
-    //     Region::UsWest2,
-    // ];
-    let client = Ec2Client::new(Region::UsWest2);
+    let regions = vec![
+        Region::ApEast1,
+        Region::ApNortheast1,
+        Region::ApNortheast2,
+        Region::ApNortheast3,
+        Region::ApSouth1,
+        Region::ApSoutheast1,
+        Region::ApSoutheast2,
+        Region::CaCentral1,
+        Region::EuCentral1,
+        Region::EuWest1,
+        Region::EuWest2,
+        Region::EuWest3,
+        Region::EuNorth1,
+        Region::MeSouth1,
+        Region::SaEast1,
+        Region::UsEast1,
+        Region::UsEast2,
+        Region::UsWest1,
+        Region::UsWest2,
+        // Region::UsGovEast1,
+        // Region::UsGovWest1,
+        // Region::CnNorth1,
+        // Region::CnNorthwest1,
+    ];
+    let mut futs = FuturesUnordered::new();
+    for region in regions {
+        // println!("Region: {}", &region.name());
+        futs.push(regional_instances(region))
+    }
+
+    while let Some(instances) = futs.next().await {
+        for instance in instances {
+            println!(
+                "Instance: {} - {} ({})",
+                instance.name,
+                instance.ip,
+                instance.region.name()
+            );
+        }
+    }
+
+    // let future_instances_by_region: Vec<_> = regions
+    //     .into_iter()
+    //     .map(|region| regional_instances(region))
+    //     .collect();
+
+    // for future_instances in future_instances_by_region {
+    //     let instances = future_instances.await;
+    //     for instance in instances {
+    //         println!("Instance: {} - {}", instance.name, instance.ip);
+    //     }
+    // }
+
+    // for future_instances in future_instances_by_region {
+    //     let instances_description = future_instances.await;
+    //     let instances = regional_instances(instances_description);
+    //     for instance in instances {
+    //         println!("Instance: {} - {}", instance.name, instance.ip);
+    //     }
+    // }
+
+    // while let Some(instances) = future_instances_by_region.into_iter().next().await {
+    //     println!("{}", "Thing");
+    //     instances.map(|instance| {
+    //         println!("Instance: {} - {}", instance.name, instance.ip);
+    //     })
+    // }
+    // let instances_us_west_2 = regional_instances(Region::UsWest2);
+    // println!("{}", Region::UsWest2.name());
+    // for instance in instances_us_west_2.await {
+    //     println!("Instance: {} - {}", instance.name, instance.ip);
+    // }
+}
+
+// async fn describe_instances(region: Region) -> DescribeInstancesResult {
+//     let client = Ec2Client::new(region);
+//     let describe_instances_input: DescribeInstancesRequest = DescribeInstancesRequest {
+//         dry_run: None,
+//         filters: None,
+//         instance_ids: None,
+//         max_results: None,
+//         next_token: None,
+//     };
+
+//     client.describe_instances(describe_instances_input)
+// }
+
+async fn regional_instances(region: Region) -> Vec<InstanceMetadata> {
+    let client = Ec2Client::new(region.clone());
     let describe_instances_input: DescribeInstancesRequest = DescribeInstancesRequest {
         dry_run: None,
         filters: None,
@@ -19,33 +110,25 @@ async fn main() {
     };
 
     let output = match client.describe_instances(describe_instances_input).await {
-        Ok(output) => {
-            println!("Got something");
-            output
-        }
-        Err(error) => {
-            eprintln!("Got error: {}", error);
-            std::process::exit(1)
+        Ok(output) => output,
+        Err(_) => {
+            eprintln!("{} - ERROR - Failed", &region.name());
+            // std::process::exit(1)
+            DescribeInstancesResult {
+                next_token: None,
+                reservations: None,
+            }
         }
     };
 
     let reservations = match output.reservations {
-        Some(reservation) => {
-            println!("Got reservation");
-            reservation
-        }
+        Some(reservations) => reservations,
         None => {
             println!("No reservation");
-            std::process::exit(1)
+            // std::process::exit(1)
+            Vec::new()
         }
     };
-
-    // let instances = reservations
-    //     .map(|reservation| match reservation.instances {
-    //         Some(instances) => instances,
-    //         None => Vec::new(),
-    //     })
-    //     .collect::<Vec<Instance>>();
 
     let mut total_instances: Vec<Instance> = Vec::new();
     for reservation in reservations {
@@ -59,7 +142,13 @@ async fn main() {
         }
     }
 
-    println!("Total instances: {}", total_instances.len());
+    println!(
+        "{}: total instances: {}",
+        &region.name(),
+        total_instances.len()
+    );
+
+    let mut instance_metadatas = Vec::new();
 
     for instance in total_instances {
         let ip_address = match &instance.public_ip_address {
@@ -91,6 +180,13 @@ async fn main() {
             None => String::from("N/A"),
         };
 
-        println!("{}: {}", name, ip_address);
+        // println!("{}: {} ({})", name, ip_address, &region.name());
+        instance_metadatas.push(InstanceMetadata {
+            name,
+            ip: String::from(ip_address),
+            region: region.clone(),
+        });
     }
+
+    instance_metadatas
 }
